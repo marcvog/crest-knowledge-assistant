218e253e8508aa879023c98a1ab7fe78761ce0e8
Marcelo Vogel <mavogel@cern.ch>
2026-08-28 07:58:07 +0000

    updated index version

--- src/crest_knowledge_assistant/models/index_version.py
+++ src/crest_knowledge_assistant/models/index_version.py
@@ -5,14 +5,16 @@ from datetime import datetime
 @dataclass
 class IndexVersion:
     version_id: str
-    git_reference: str
+    source_git_reference: str
+    application_git_reference: str
     indexing_date: str
     entity_count: int
 
     def to_dict (self) -> dict:
         dictionary = {
                 "version_id" : self.version_id,
-                "git_reference" : self.git_reference,
+                "source_git_reference" : self.source_git_reference,
+                "application_git_reference" : self.application_git_reference,
                 "indexing_date" : self.indexing_date,
                 "entity_count" : self.entity_count
         }
@@ -22,7 +24,8 @@ class IndexVersion:
     def from_dict (cls, data: dict) -> "IndexVersion":
         version = cls(
             version_id=data["version_id"],
-            git_reference=data["git_reference"],
+            source_git_reference=data["source_git_reference"],
+            application_git_reference=data["application_git_reference"],
             indexing_date=data["indexing_date"],
             entity_count=data["entity_count"]
         )
